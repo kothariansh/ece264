f1c783c1e3ce46252e547fbaafa41379a872679c
Ansh <anshkothari558@gmail.com>
2025-04-18 18:23:19 -0400

    Update populatingNextRightPointersinEachNode.c

--- populatingNextRightPointersinEachNode.c
+++ populatingNextRightPointersinEachNode.c
@@ -24,3 +24,44 @@ struct Node* connect(struct Node* root) {
     
     return root;
 }
+
+
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     struct Node *left;
+ *     struct Node *right;
+ *     struct Node *next;
+ * };
+ */
+
+void connect_helper(struct Node *root) 
+{
+    if (!root)
+        return;
+
+    static struct Node dummy;
+    dummy.next = 0;
+
+    struct Node *last = &dummy;
+    while(root)
+    {
+        if (root->left)
+            last = last->next = root->left;
+        if (root->right)
+            last = last->next = root->right;
+
+        root = root->next;
+    }
+
+    connect_helper(dummy.next);
+}
+
+
+struct Node* connect(struct Node* root) 
+{
+	connect_helper(root);
+
+    return root;
+}
